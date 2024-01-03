f7fdafa22708edd339b8d0082c74983e205f830d
john-okojere <johnokojere08@gmail.com>
2024-01-03 22:13:02 +0100

    modified:   BLOG/settings.py 	renamed:    home/static/asset/css/form.css -> users/static/css/form.css 	renamed:    home/static/asset/css/profile.css -> users/static/css/profile.css

--- BLOG/settings.py
+++ BLOG/settings.py
@@ -79,16 +79,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'BLOG.wsgi.application'
 
 
-# Database
+
+db_path = os.path.join(os.pardir, 'db.sqlite3')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': db_path,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
